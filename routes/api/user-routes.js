6e29d26cdbc8ae4f1d651b7c424bdd53d44c930e
BrandonDaws <velarde.brandon02@gmail.com>
2022-05-24 19:00:34 -0500

    updated GET request for users to hide password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,8 +5,16 @@ const { User } = require('../../models');
 // GET /api/users
 router.get('/', (req, res) => {
     // Access our User model and run .findAll() method)
-  User.findAll()
-  .then(dbUserData => res.json(dbUserData))
+  User.findAll({
+      attributes: {exclude: ['password'] }
+  })
+  .then(dbUserData => {
+      if (!dbUserData){
+          res.status(404).json({message: 'No user found with this id'});
+          return;
+      }
+  res.json(dbUserData);
+    })
   .catch(err => {
     console.log(err);
     res.status(500).json(err);
